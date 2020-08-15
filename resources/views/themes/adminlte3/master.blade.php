7da1ffb45e5e4e189fb215715ac241e3cb0ed028
Heru Rahmat Akhnuari <eyubalzary@gmail.com>
2020-08-16 01:47:15 +0700

    remove comment

--- resources/views/themes/adminlte3/master.blade.php
+++ resources/views/themes/adminlte3/master.blade.php
@@ -2,10 +2,6 @@
 $theme_url=url('assets/themes/adminlte3/').'/';
 @endphp
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html lang="en">
 
 <head>
@@ -55,7 +51,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="{{ route('core.account.dashboard') }}" class="brand-link">
-                <img src="{{ url('logo.svg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="{{ zeus_logo() }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">{{ meta_read('app_name') }}</span>
             </a>
 
