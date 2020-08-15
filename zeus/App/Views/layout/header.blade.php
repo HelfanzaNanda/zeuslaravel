74b6041aefa0646a193b1def83e478a83231558d
Heru Rahmat Akhnuari <eyubalzary@gmail.com>
2020-08-16 01:47:43 +0700

    add favicon and fixing template layout template

--- zeus/App/Views/layout/header.blade.php
+++ zeus/App/Views/layout/header.blade.php
@@ -2,6 +2,7 @@
 echo add_css(asset('assets/css/style.css'));
 echo add_js(asset('assets/js/script.js'));
 @endphp
+<link rel="shortcut icon" href="{{ zeus_favicon() }}"/>
 <script>
     var base_url = "{{ url('/') }}";
 </script>
